da59c294d4c93709fe951c967eb52fd9f989dbcd
ketangote <ketan.gote@metamagic.in>
2019-02-14 00:08:41 +0530

    Changes: Update

--- src/app/dashboardthree/dashboardthree.component.ts
+++ src/app/dashboardthree/dashboardthree.component.ts
@@ -6,10 +6,11 @@ import { Component } from "@angular/core";
 })
 export class DashboardThreeComponent {
 
-    lastTenMonth: any[];
-
+    lastTenMonthRevenue: any[];
+    lastTenMonthUsers : any[];
+    
     constructor(){
-        this.lastTenMonth = [
+        this.lastTenMonthRevenue = [
             [{ "datatype": "number", "label": 'Month' }, { "datatype": "number", "label": 'Revenue' },
             ],
             ['JAN', 10],
@@ -26,5 +27,22 @@ export class DashboardThreeComponent {
             ['DEC', 40],
             
           ];
+
+        this.lastTenMonthUsers = [
+            ['Months','County'],
+            ['JAN', 10],
+            ['FEB', 30],
+            ['MAR', 20],
+            ['APR', 10],
+            ['MAY', 30],
+            ['JUN', 20],
+            ['JUL', 20],
+            ['AUG', 30],
+            ['SEP', 40],
+            ['OCT', 30],
+            ['NOV', 30],
+            ['DEC', 40],
+            
+          ]
     }
 }
