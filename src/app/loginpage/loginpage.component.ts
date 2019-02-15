89fa8171b7dad31b9e206504370110bc2f1bdeef
ketangote <ketan.gote@metamagic.in>
2019-02-15 11:29:57 +0530

    Changes: Removed unwanted code

--- src/app/loginpage/loginpage.component.ts
+++ src/app/loginpage/loginpage.component.ts
@@ -3,17 +3,19 @@
  */
 import {Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'login',
-  templateUrl: './loginpage.component.html',
-  styleUrls: ['./loginpage.component.css']
+  templateUrl: './loginpage.component.html'
 })
 export class LoginPageComponent implements OnInit {
+
   constructor(private router: Router) {
   }
-  ngOnInit() {
-    
+
+  ngOnInit() {  
   }
+
   onLoginBtnClick() {
     this.router.navigate(['/home']);
   }
