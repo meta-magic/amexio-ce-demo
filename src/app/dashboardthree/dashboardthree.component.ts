1873cdb2718fa233af94cdfa21b090686750a937
ketangote <ketan.gote@metamagic.in>
2019-02-15 12:36:03 +0530

    Changes: Code refactored

--- src/app/dashboardthree/dashboardthree.component.ts
+++ src/app/dashboardthree/dashboardthree.component.ts
@@ -1,67 +1,33 @@
 import { Component } from "@angular/core";
+import { HTTPService } from "../service/http.service";
 
 @Component({
-    selector : 'dashboard-three',
-    templateUrl : './dashboardthree.component.html'
+  selector: 'dashboard-three',
+  templateUrl: './dashboardthree.component.html'
 })
 export class DashboardThreeComponent {
 
-    lastTenMonthRevenue: any[];
-    lastTenMonthUsers : any[];
-    globalVists : any[];
-    globalVistsData : any[];
+  lastTenMonthRevenue: any[] = [];
+  lastTenMonthUsers: any[] = [];
+  globalVists: any[] = [];
+  globalVistsData: any[] = [];
 
-    constructor(){
-        this.lastTenMonthRevenue = [
-            [{ "datatype": "number", "label": 'Month' }, { "datatype": "number", "label": 'Revenue' },
-            ],
-            ['JAN', 10],
-            ['FEB', 30],
-            ['MAR', 20],
-            ['APR', 10],
-            ['MAY', 30],
-            ['JUN', 20],
-            ['JUL', 20],
-            ['AUG', 30],
-            ['SEP', 40],
-            ['OCT', 30],
-            ['NOV', 30],
-            ['DEC', 40],
-            
-          ];
+  constructor(private httpService: HTTPService) {
+  }
 
-        this.lastTenMonthUsers = [
-            ['Months','County'],
-            ['JAN', 10],
-            ['FEB', 30],
-            ['MAR', 20],
-            ['APR', 10],
-            ['MAY', 30],
-            ['JUN', 20],
-            ['JUL', 20],
-            ['AUG', 30],
-            ['SEP', 40],
-            ['OCT', 30],
-            ['NOV', 30],
-            ['DEC', 40],
-            
-          ];
+  ngOnInit() {
+    this.fetchData();
+  }
 
-          this.globalVists=[
-            ['Country', 'Popularity'],
-            ['Germany', 200],
-            ['United States', 400],
-            ['France', 550],
-            ['RU', 350],
-            ['India',300]
-          ];
 
-          this.globalVistsData=[
-            {'country':'Germany','count':200, 'flag':'DE-32.png'},
-            {'country':'United States','count':400, 'flag':'US-32.png'},
-            {'country':'France','count':500, 'flag':'FR-32.png'},
-            {'country':'Russia','count':350, 'flag':'RU-32.png'},
-            {'country':'India','count':300, 'flag':'IN-32.png'}
-          ];
-    }
+  fetchData() {
+    let response: any;
+    this.httpService.fetch("assets/jsondata/dashboardthree.json").subscribe((resp: any) => {
+      debugger;
+      this.lastTenMonthRevenue = resp.lastTenMonthRevenue;
+      this.lastTenMonthUsers = resp.lastTenMonthUsers;
+      this.globalVists = resp.globalVists;
+      this.globalVistsData = resp.globalVistsData;
+    });
+  }
 }
