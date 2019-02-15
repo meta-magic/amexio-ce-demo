e0006d0454a5449ce5e638f35a1484a32d9c46c7
ketangote <ketan.gote@metamagic.in>
2019-02-15 12:54:57 +0530

    Changes: refactored

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { HTTPService } from "../service/http.service";
 
 @Component({
     selector : 'time-line',
     templateUrl : './timeline.component.html'
 })
 export class TimelineComponent implements OnInit{
-
-    constructor(public http: HttpClient) {} 
+    
     timeLineData: any;
+    
+    constructor(public http: HTTPService) {} 
+    
 
     ngOnInit() {
-        this.http.get('assets/jsondata/timeline.json')
+        this.http.fetch('assets/jsondata/timeline.json')
         .subscribe(response => {
             this.timeLineData= response;
-            console.log('kedar', this.timeLineData);
         });
     }
 }
